9d7cd6ddd46a60c9c2eb7bc60fb9ee886186384c
mukiva <mukiva2261@gmail.com>
2024-06-21 02:12:35 +0300

    Добавил TicketFoundApplication

---
+++ app/src/main/java/com/github/mukiva/ticketfound/TicketFoundApp.kt
@@ -0,0 +1,7 @@
+package com.github.mukiva.ticketfound
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TicketFoundApp : Application()
---
+++ app/src/main/java/com/github/mukiva/ticketfound/di/DataModule.kt
@@ -0,0 +1,11 @@
+package com.github.mukiva.ticketfound.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataModule {
+    // TODO("Implement [api, data] and optional [database]")
+}
